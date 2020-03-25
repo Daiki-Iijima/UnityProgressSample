f14946964f59769e6de164fedfc900412fc03940
daiki_iijima <daiki.applecreate@gmail.com>
2020-03-26 00:42:29 +0900

    Sliderで進行状況を確認できるように

---
+++ Assets/ProgressExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressExample : MonoBehaviour
+{
+    public int MaxCount;
+
+    [SerializeField] private Slider progressSlider;
+    
+    void Start()
+    {
+        StartCoroutine(HeavyMethod(new Progress<float>(dd)));
+    }
+
+    private void dd(float s)
+    {
+        progressSlider.value = s;
+    }
+    
+    private IEnumerator HeavyMethod(IProgress<float> progress)
+    {
+        for (int i = 0; i <= MaxCount; i++)
+        {
+            progress.Report((float)i/MaxCount);
+            Debug.Log(i);
+            yield return null;
+        }
+            
+        yield break;
+    }
+}
